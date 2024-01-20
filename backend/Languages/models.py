fa875161ded0908990991ee5b8db692fcde14272
EmirAlpAksungur <77407633+EmirAlpAksungur@users.noreply.github.com>
2024-01-20 03:50:06 +0300

    BL- ix docker

--- backend/Languages/models.py
+++ backend/Languages/models.py
@@ -4,4 +4,5 @@ from django.db import models
 
 class Languages(models.Model):
     LanguageId = models.CharField(primary_key = True)
+    LanguageCode = models.CharField(max_length = 5)
     LanguageName = models.CharField(max_length = 100)
--- backend/Translations/models.py
+++ backend/Translations/models.py
@@ -5,6 +5,7 @@ from TextContent.models import TextContent
 # Create your models here.
 
 class Translations(models.Model):
+    TranslationsId = models.CharField(primary_key = True)
     TextContentId = models.ForeignKey(TextContent, on_delete=models.CASCADE)
     LanguageId = models.ForeignKey(Languages, on_delete=models.CASCADE)
     Translations = models.CharField(max_length = 200)
